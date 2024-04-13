46578673260ab0ce2e87ec2812cd56291dddf521
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-04-13 10:41:00 +0300

    backend promis tasks

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,11 +2,11 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
-      status: 200,
-      body: 'Success',
-     });
+        status: 200,
+        body: 'Success',
+      });
     } else {
       reject(new Error('The fake API is not working currently'));
-      }
+    }
   });
 }
