710c76cf59690909803982ff4d50a7ef33c546d1
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-04-17 23:12:27 +0300

    interface and class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,35 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+interface Student {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+    
+    constructor (firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working'
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
