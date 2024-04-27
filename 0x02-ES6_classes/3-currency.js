5f6fe330f74aa827ab08f28d84c81fdce2593796
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-04-27 18:37:44 +0300

    methods

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,28 @@
-expo
+// implementation of Currency class
+
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(citem) {
+    this._code = citem;
+  }
+
+  get name() {
+    return this.code;
+  }
+
+  set name(nitem) {
+    this._name = nitem;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
